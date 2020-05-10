fe643a33417a50e25590b2b8ebd2f18f50a5672b
Rohan Mutalik <rmutalik@gmail.com>
2020-05-09 22:12:24 -0400

    Refactored data loading process

--- MIMIC-III.py
+++ MIMIC-III.py
@@ -1,12 +1,29 @@
 # Import libraries
+import os, os.path
+import shutil
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load data
-base_path = "C:\\Users\\rmutalik\\Desktop\\mimic-iii-clinical-database-1.4\\"
 
-df_admissions = pd.read_csv(base_path + "ADMISSIONS.csv")
-df_callout = pd.read_csv(base_path + "CALLOUT.csv")
-df_caregivers = pd.read_csv(base_path + "CAREGIVERS.csv")
-df_cptevents = pd.read_csv(base_path + "CPTEVENTS.csv")
+# Load Data
+csv_folder = r'C:\Users\rmutalik\Desktop\mimic-iii-clinical-database-1.4'
+df_dict = {}
+
+# Loop through all files in folders
+for file in os.listdir(csv_folder):
+    
+    if file.endswith(".csv"):
+        name = os.path.splitext(file)[0]
+
+        # If file size is greater than ~100MB, skip it
+        MAX_FILE_SIZE = 100000000
+        physical_file = os.path.join(csv_folder, file)
+        statinfo = os.stat(physical_file)
+        print(statinfo)            
+        if statinfo.st_size > MAX_FILE_SIZE:
+           print(file, statinfo.st_size)
+           continue
+        else:
+            df_dict[name] = pd.read_csv(physical_file)
